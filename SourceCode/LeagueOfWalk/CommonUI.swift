bd9c9010cbff414e884a425143f0cc88611e590b
devyhan93 <devyhan93@gmail.com>
2020-07-14 20:12:36 +0900

    docs: Common dir 생성
    공통 속성 정의를 위한 Common디렉토리 생성 및 CommonUI.swift파일 생성
    기본 배경색 설정

---
+++ SourceCode/LeagueOfWalk/CommonUI.swift
@@ -0,0 +1,18 @@
+//
+//  CommonUI.swift
+//  LeagueOfWalk
+//
+//  Created by 요한 on 2020/07/14.
+//  Copyright © 2020 김광수. All rights reserved.
+//
+
+import UIKit
+
+struct CommonUI {
+  
+  // Colors
+  
+  static let backgroundColor: UIColor = UIColor(red: 17, green: 17, blue: 17, alpha: 1)
+  static let pointColor: UIColor = UIColor(red: 6, green: 28, blue: 37, alpha: 1)
+  static let edgeColor: UIColor = UIColor(red: 194, green: 143, blue: 44, alpha: 1)
+}
