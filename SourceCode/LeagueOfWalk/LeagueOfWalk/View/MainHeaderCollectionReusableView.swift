798d40009fa593cfcf2999daa0ab0375aa771e00
devyhan93 <devyhan93@gmail.com>
2020-07-15 19:57:09 +0900

    feat: 알 모션 추가
    알의 이미지를 터치하면 에니메이션을 줌

--- SourceCode/LeagueOfWalk/LeagueOfWalk/View/MainHeaderCollectionReusableView.swift
+++ SourceCode/LeagueOfWalk/LeagueOfWalk/View/MainHeaderCollectionReusableView.swift
@@ -11,6 +11,14 @@ import SnapKit
 
 class MainHeaderCollectionReusableView: UICollectionReusableView {
   
+  var count = 0
+  var ments: String? {
+    didSet {
+      openMent.text = ments
+    }
+  }
+  
+  var isHoldingImage = false
   static let identifier = "MainHeaderCollectionReusableView"
   private let heroImageView: UIImageView = {
     let imageView = UIImageView()
@@ -19,13 +27,16 @@ class MainHeaderCollectionReusableView: UICollectionReusableView {
     return imageView
   }()
   
-  private let openButton: UIButton = {
-    let button = UIButton()
-    button.setTitle("눌러서 확인하기", for: .normal)
-    button.setTitleColor(.white, for: .normal)
-    button.addTarget(self, action: #selector(didTabOpenButton), for: .touchUpInside)
+  private let openMent: UILabel = {
+    let label = UILabel()
+    label.text = "알을 눌러 확인하세요"
+    label.textColor = .white
+    label.font = UIFont(
+      name: CommonUI.CustonFonts.koFont.rawValue,
+      size: CommonUI.FontSize.Large.rawValue
+    )
     
-    return button
+    return label
   }()
   
   // MARK: - LifeCycle
@@ -40,18 +51,13 @@ class MainHeaderCollectionReusableView: UICollectionReusableView {
   
   private func setUI() {
     
-    openButton.titleLabel?.font = UIFont(
-      name: CommonUI.CustonFonts.koFont.rawValue,
-      size: 20
-    )
-    
-    [heroImageView, openButton].forEach {
+    [heroImageView, openMent].forEach {
       self.addSubview($0)
     }
     heroImageView.snp.makeConstraints {
       $0.centerX.centerY.equalTo(self)
     }
-    openButton.snp.makeConstraints {
+    openMent.snp.makeConstraints {
       $0.bottom.equalTo(heroImageView)
       $0.centerX.equalTo(heroImageView)
     }
@@ -59,9 +65,71 @@ class MainHeaderCollectionReusableView: UICollectionReusableView {
   
   // MARK: - Action
   
-  @objc private func didTabOpenButton() {
-    print("Open")
-    // Open Animation
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesBegan(touches, with: event)
+    
+    switch count {
+    case 0...3:
+      didTabEggRotatedAnimation()
+      ments = "방금 알이 움직인건가요?"
+    case 4...12:
+      didTabEggRotatedAnimation()
+      ments = "무엇인가 안에서 움직이고 있네요 !!"
+    case 12...19:
+      didTabEggRotatedAnimation()
+      didTabEggShakeAnimation()
+      ments = "움직임이 커졌네요 !!"
+    case 19...26:
+      didTabEggRotatedAnimation()
+      didTabEggShakeAnimation()
+      ments = "곧 무엇인가 밖으로 나올것 같아요 !!"
+    default:
+      print("Open")
+      ments = "당신의 전설이가 알을깨고 나왔습니다 !!"
+      count = 0
+    }
+    
+    count += 1
+    print(count)
+    
+  }
+  
+  private func didTabEggRotatedAnimation() {
+    let random: CGFloat = CGFloat(drand48()) - 0.5
+    
+    UIView.animateKeyframes(withDuration: 0.25, delay: 0, animations: {
+      UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.2, animations: {
+        self.heroImageView.transform = self.heroImageView.transform.rotated(by: random)
+      })
+      UIView.addKeyframe(withRelativeStartTime: 0.2, relativeDuration: 0.3, animations: {
+        self.heroImageView.transform = .identity
+      })
+      UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.3, animations: {
+        self.heroImageView.transform = self.heroImageView.transform.rotated(by: -random)
+      })
+      UIView.addKeyframe(withRelativeStartTime: 0.8, relativeDuration: 0.2, animations: {
+        self.heroImageView.transform = .identity
+      })
+    })
+  }
+  
+  private func didTabEggShakeAnimation() {
+    UIView.animateKeyframes(withDuration: 0.25, delay: 0, animations: {
+      UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.2, animations: {
+        self.heroImageView.center.x -= 8
+      })
+      UIView.addKeyframe(withRelativeStartTime: 0.2, relativeDuration: 0.3, animations: {
+//        self.heroImageView.transform = .identity
+        self.heroImageView.center.x += 16
+      })
+      UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.3, animations: {
+        self.heroImageView.center.x -= 16
+      })
+      UIView.addKeyframe(withRelativeStartTime: 0.8, relativeDuration: 0.2, animations: {
+        self.heroImageView.transform = .identity
+        self.heroImageView.center.x += 8
+      })
+    })
   }
   
   required init?(coder: NSCoder) {
