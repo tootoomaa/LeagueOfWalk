1912456c0c0986c1b47b618a34f41ed6f7e5870d
devyhan93 <devyhan93@gmail.com>
2020-07-17 01:08:12 +0900

    docs: Regular size font 추가

---
+++ SourceCode/LeagueOfWalk/LeagueOfWalk/View/CustomView/WalkProgressView.swift
@@ -0,0 +1,21 @@
+//
+//  WalkProgressView.swift
+//  LeagueOfWalk
+//
+//  Created by 요한 on 2020/07/16.
+//  Copyright © 2020 김광수. All rights reserved.
+//
+
+import UIKit
+
+class WalkProgressView: UIProgressView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SourceCode/LeagueOfWalk/LeagueOfWalk/View/ProgressView.swift
@@ -0,0 +1,21 @@
+//
+//  ProgressView.swift
+//  LeagueOfWalk
+//
+//  Created by 요한 on 2020/07/16.
+//  Copyright © 2020 김광수. All rights reserved.
+//
+
+import UIKit
+
+class ProgressView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
